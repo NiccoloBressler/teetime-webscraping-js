b839b083a0cfc2ff427348d0dfcc749bebccd7ca
Nicco <bresslerniccolo@gmail.com>
2022-01-15 02:05:27 -0500

    Added functionality to webscrape player slots available for certain teetimes and fixed bugs where functions were printing nothing

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ async function scrapeCourse(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-
+    
+    // Returns times in new file
     const times = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#hot-deal-zone > div > div > time > div')).map(x => x.textContent);
+        return Array.from(document.querySelectorAll('#ttresults > section.search-results.location.content > div > section > a > time')).map(x => x.textContent);
     });
+
     await fs.writeFile('teetimes.txt', times.join('\r\n'));
     
+    // Returns prices in new file
+    const prices = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('#ttresults > section.search-results.location.content > div > section > a > div > p.price')).map(x => x.textContent);
+    });
+
+    await fs.writeFile('prices.txt', prices.join('\r\n'));
+
+    // Returns player slots in new file
+    const players = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('#ttresults > section.search-results.location.content > div > section > a > div > p.tt-detail > span.golfers-available')).map(x => x.textContent);
+    });
+
+    await fs.writeFile('players.txt', players.join('\r\n'));
+
     await browser.close();
 }
 
